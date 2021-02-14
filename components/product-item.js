9b0cda4cf55d86f447a3086786ca4ef2380161f9
Matias Lin <melin@ucsd.edu>
2021-02-13 22:11:16 -0800

    Finished product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,26 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    self = super();
+    // Attach a shadow root to <product-item>
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./styles/styles.css" />
+      <li class="product" id="curr">
+          <img
+            id="image"
+            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            alt="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+          />
+          <p class="title" id="title">
+            "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+          </p>
+          <p class="price" id="price">"$109.95</p>
+          <button id="button" onclick="alert('Added to Cart!')">Add to Cart</button>
+      </li>
+    `;
+  }
 }
 
 customElements.define('product-item', ProductItem);
